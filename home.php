078429e8063733d793b157627cfbb25a0af13e31
vpgourdin <vpgourdin@gmail.com>
2017-12-19 12:44:02 -0700

    Initial Commit

---
+++ home.php
@@ -0,0 +1,25 @@
+<?php get_header(); ?>
+<section>
+	<div class="home">
+		<h1>
+			Sivan Weitz
+		</h1>
+		<p>
+			What I do - Write a quick sentence here summarizing what you do
+ 		</p>
+	</div>
+	<div class="nav">
+		<a href="home">Home /</a>
+		<a href="about">About Me /</a>
+		<a href="portfolio">Portfolio /</a>
+		<a href="work">Work with me /</a>
+	</div>
+	<div class="social">
+		<a href="http://instagram.com">Insta</a>
+		<a href="http://twitter.com">Twitter</a>
+		<a href="http://pinterest.com">Pinterest</a>
+		<a href="http://youtube.com">Youtube</a>
+	</div>
+</section>
+
+<?php get_footer(); ?>
